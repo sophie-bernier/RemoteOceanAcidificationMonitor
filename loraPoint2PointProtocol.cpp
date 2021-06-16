5d2ed233560694e9058cf0b9b1cd733bf1178bfb
SB <sebastien.bernier@ucalgary.ca>
2021-06-16 14:00:28 -0600

    Moved common functions between LoRaRangeTest sketches into loraPoint2PointProtocol library. setupRadio forceRadioReset setFrequencyChannel setSpreadingFactor setBandwidth buildStringfromSerial serviceTx serviceRx

--- loraPoint2PointProtocol.cpp
+++ loraPoint2PointProtocol.cpp
@@ -1,32 +1,215 @@
 // loraPoint2PointProtocol.cpp
 
 #include <Arduino.h>
-#include "loraPoint2PointProtocol.h"
+#include <loraPoint2PointProtocol.h>
 
-loraPoint2Point
+//----------------------
+// Function Definitions
+//----------------------
 
-sendLinkAdapt ()
-
-
-uint8_t loraPoint2Point::serviceMsg (uint8_t* buf, 
-                                     uint8_t* len,
-                                     uint8_t* from,
-                                     uint8_t* to,
-                                     uint8_t* id,
-                                     uint8_t* flags)
+void loraPoint2Point::setupRadio ()
 {
-  switch (buf[0])
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+  Serial.begin(USB_SERIAL_BAUD);
+  while (!Serial)
   {
-    case msgType_data:
-      serviceMsgData(buf[1], len - 1, from, to, id, flags);
-      break;
-    case msgType_linkAdapt:
-      serviceMsgLinkAdapt(buf[1], len - 1);
-      break;
+    delay(1);
   }
-  return 0;
+  delay(100);
+  Serial.println("Feather LoRa Range Test - Base!");
+  
+  forceRadioReset();
+
+  while (!rf95.init())
+  {
+    Serial.println("LoRa radio init failed");
+    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  #if (USE_RH_RELIABLE_DATAGRAM > 0)
+  while (!rhReliableDatagram.init())
+  {
+    Serial.println("Manager init failed");
+    while (1);
+  }
+  Serial.println("Manager init OK!");
+  #endif // USE_RH_RELIABLE_DATAGRAM
+
+  rf95.setSpreadingFactor(RFM95_DFLT_SF);
+  rf95.setSignalBandwidth(RFM95_DFLT_BW_Hz);
+  rf95.setTxPower(RFM95_DFLT_TX_POWER_dBm, false);
 }
 
+void loraPoint2Point::forceRadioReset ()
+{
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+}
 
+void loraPoint2Point::loraPoint2Point::setSpreadingFactor (spreadingFactor_t spreadingFactor)
+{
+  if (spreadingFactor >= NUM_spreadingFactors)
+  {
+    spreadingFactor = spreadingFactor_sf7;
+  }
+  uint8_t spreadingFactorToSet = spreadingFactorTable[spreadingFactor];
+  rf95.setSpreadingFactor(spreadingFactorToSet);
+  Serial.print("Set SF to: ");
+  Serial.println(spreadingFactorToSet);
+}
 
-Hello World!
+void loraPoint2Point::setBandwidth (signalBandwidth_t bandwidth)
+{
+  if (bandwidth >= NUM_signalBandwidths)
+  {
+    bandwidth = signalBandwidth_125kHz;
+  }
+  uint32_t bandwidthToSet = signalBandwidthTable[bandwidth];
+  rf95.setSignalBandwidth(bandwidthToSet);
+  Serial.print("Set BW to: ");
+  Serial.println(bandwidthToSet);
+}
+
+void loraPoint2Point::setFrequencyChannel (frequencyChannel_t frequencyChannel)
+{
+  if (frequencyChannel >= NUM_frequencyChannels)
+  {
+    frequencyChannel = frequencyChannel_500kHz_Uplink_0;
+  }
+  float frequencyToSet = ((float)(frequencyChannelTable[frequencyChannel]))/10;
+  if (!rf95.setFrequency(frequencyToSet))
+  {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: ");
+  Serial.println(frequencyToSet);
+}
+
+uint8_t loraPoint2Point::buildStringFromSerial (Serial_* dataPort)
+{
+  uint8_t retval;
+  char inputChar;
+  while (dataPort->available())
+  {
+    inputChar = dataPort->read();
+    Serial.print(inputChar);
+    retval = buildStringFromSerialInner(inputChar);
+  }
+  return retval;
+}
+
+uint8_t loraPoint2Point::buildStringFromSerial (Uart* dataPort)
+{
+  uint8_t retval;
+  char inputChar;
+  while (dataPort->available())
+  {
+    inputChar = dataPort->read();
+    Serial.print(char(inputChar));
+    retval = buildStringFromSerialInner(inputChar);
+  }
+  return retval;
+}
+
+uint8_t loraPoint2Point::buildStringFromSerialInner (char inputChar)
+{
+  // Add chars to ignore to this.
+  if ((inputChar != '\n'
+       && inputChar != '\r')
+      && txBufIdx < RH_RF95_MAX_MESSAGE_LEN)
+  {
+    txBuf[txBufIdx] = inputChar;
+    txBufIdx++;
+  }
+  // Special start key
+  if (inputChar == '$')
+  {
+    txBuf[txBufIdx] = 27;
+    txBufIdx++;
+  }
+  // \n triggers sending
+  if (inputChar == '\n')
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+void loraPoint2Point::serviceTx (uint8_t destAddress)
+{
+  if (txBufIdx > 0)
+  {
+    Serial.print("Attempting to transmit: \"");
+    for (uint8_t i = 0; i < txBufIdx; i++)
+    {
+      Serial.print(char(txBuf[i]));
+    }
+    Serial.println('\"');
+    rf95.waitCAD();
+    #if (USE_RH_RELIABLE_DATAGRAM > 0)
+    if (rhReliableDatagram.sendtoWait(txBuf, txBufIdx, destAddress) == true)
+    {
+      Serial.println("Sent successfully & acknowleged!");
+    }
+    txBufIdx = 0;
+    #else // USE_RH_RELIABLE_DATAGRAM
+    rf95.send(txBuf, txBufIdx);
+    txBufIdx = 0;
+    rf95.waitPacketSent();
+    #endif  // USE_RH_RELIABLE_DATAGRAM
+    Serial.println("Sent successfully!");
+  }
+  else
+  {
+    Serial.print("Nothing to transmit: TX buffer empty.");
+  }
+}
+
+void loraPoint2Point::serviceRx ()
+{
+  if (
+      #if (USE_RH_RELIABLE_DATAGRAM > 0)
+      rhReliableDatagram.recvfromAckTimeout(rxBuf,
+                                     &rxBufLen,
+                                     500,
+                                     &rxSrcAddr,
+                                     &rxDestAddr,
+                                     &rxId,
+                                     &rxFlags)
+      #else // USE_RH_RELIABLE_DATAGRAM
+      rf95.recv(rxBuf, &rxBufLen)
+      #endif  // USE_RH_RELIABLE_DATAGRAM
+     )
+  {
+    switch (rxBuf[0])
+    {
+      default:
+      Serial.print("Received: \"");
+      for (uint8_t i = 0; i < rxBufLen; i++)
+      {
+        Serial.print(char(rxBuf[i]));
+      }
+      Serial.println("\"");
+      rxBufLen = RH_RF95_MAX_MESSAGE_LEN;
+      break;
+    }
+  }
+}
+
+/*
+uint8_t serviceMsg (uint8_t* buf,
+                    uint8_t* len,
+                    uint8_t srcAddr,
+                    uint8_t destAddr,
+                    uint8_t id,
+                    uint8_t flags);
+*/
--- loraPoint2PointProtocol.h
+++ loraPoint2PointProtocol.h
@@ -2,16 +2,41 @@
 
 #ifndef LORA_POINT_2_POINT_PROTOCOL_H
 #define LORA_POINT_2_POINT_PROTOCOL_H
+#include <Arduino.h>
 #include <SPI.h>
 #include <RH_RF95.h>
 #include <RHReliableDatagram.h>
 
+#define RFM95_CS  8
+#define RFM95_RST 4
+#define RFM95_INT 3
+
+#define USB_SERIAL_BAUD 115200
+
+// The default transmitter power is 13dBm, using PA_BOOST.
+// If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+// you can set transmitter powers from 5 to 23 dBm:
+#define RFM95_DFLT_FREQ_MHz     902.5
+#define RFM95_DFLT_SF           7
+#define RFM95_DFLT_TX_POWER_dBm 15
+#define RFM95_DFLT_BW_Hz        500000
+
+#define RH_RF95_MAX_MESSAGE_LEN 128
+
+#define USE_RH_RELIABLE_DATAGRAM true
+
+//-----------------------------------------
+// Message Type and Structure Declarations
+//-----------------------------------------
+
 enum msgType_t
 {
+  msgType_undefined,
   msgType_dataRequest,
   msgType_linkChangeRequest,
   msgType_wakeRequest,
-  msgType_sleepRequest
+  msgType_sleepRequest,
+  NUM_msgTypes
 };
 
 enum spreadingFactor_t
@@ -21,47 +46,115 @@ enum spreadingFactor_t
   spreadingFactor_sf9,
   spreadingFactor_sf10,
   spreadingFactor_sf11,
-  spreadingFactor_sf12
+  spreadingFactor_sf12,
+  NUM_spreadingFactors
 };
+
+extern uint8_t spreadingFactorTable [];
 
 enum signalBandwidth_t
 {
   signalBandwidth_125kHz,
   signalBandwidth_250kHz,
   signalBandwidth_500kHz,
-  signalBandwidth_625kHz
+  signalBandwidth_625kHz,
+  NUM_signalBandwidths
 };
+
+extern uint32_t signalBandwidthTable [];
+
+enum frequencyChannel_t
+{
+  frequencyChannel_500kHz_Uplink_0,
+  frequencyChannel_500kHz_Uplink_1,
+  frequencyChannel_500kHz_Uplink_2,
+  frequencyChannel_500kHz_Uplink_3,
+  frequencyChannel_500kHz_Uplink_4,
+  frequencyChannel_500kHz_Uplink_5,
+  frequencyChannel_500kHz_Uplink_6,
+  frequencyChannel_500kHz_Uplink_7,
+  frequencyChannel_500kHz_Downlink_0,
+  frequencyChannel_500kHz_Downlink_1,
+  frequencyChannel_500kHz_Downlink_2,
+  frequencyChannel_500kHz_Downlink_3,
+  frequencyChannel_500kHz_Downlink_4,
+  frequencyChannel_500kHz_Downlink_5,
+  frequencyChannel_500kHz_Downlink_6,
+  frequencyChannel_500kHz_Downlink_7,
+  NUM_frequencyChannels
+};
+
+extern uint16_t frequencyChannelTable [];
+
+//-----------------------
+// Class Declarations
+//-----------------------
 
 class loraPoint2Point
 {
-  public;
-    loraPoint2Point (uint8_t thisAddress,
-                     uint8_t (*_serviceMsgData)(uint8_t* buf, 
-                                                uint8_t* len,
-                                                uint8_t srcAddr,
-                                                uint8_t destAddr,
-                                                uint8_t id,
-                                                uint8_t flags),
-                     uint8_t (*_serviceAck)(uint8_t srcAddr));
-    sendLinkChangeRequest (uint8_t destAddr;
-                           spreadingFactor_t spreadingFactor,
-                           signalBandwidth_t signalBandwidth,
-                           int8_t txPower);
-    checkForMessages ();
-    sendSleepRequest (uint8_t destAddr);
-    sendWakeRequest (uint8_t destAddr);
-    goToSleep ();
-  private;
-    uint8_t (*serviceMsgData)()
-    uint8_t (*serviceMsgLinkAdapt)()
+  public:
+    loraPoint2Point (uint8_t _thisAddress,
+                     uint8_t rfm95CS,
+                     uint8_t rfm95Int):
+                     thisAddress{_thisAddress},
+                     rf95(rfm95CS, rfm95Int),
+                     rhReliableDatagram(rf95, _thisAddress)
+                     {}
+    
+    uint8_t txBuf [RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t txBufIdx = 0;
+    uint8_t txSrcAddr = 0;
+    uint8_t txDestAddr = 0;
+    uint8_t txId = 0;
+    uint8_t txFlags = 0;
+    msgType_t txMsgType = msgType_undefined;
+
+    uint8_t rxBuf [RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t rxBufLen = RH_RF95_MAX_MESSAGE_LEN;
+    uint8_t rxSrcAddr = 0;
+    uint8_t rxDestAddr = 0;
+    uint8_t rxId = 0;
+    uint8_t rxFlags = 0;
+    msgType_t rxMsgType = msgType_undefined;
+    
+    void setupRadio ();
+    void forceRadioReset ();
+    void setFrequencyChannel (frequencyChannel_t frequencyChannel);
+    void setSpreadingFactor  (spreadingFactor_t spreadingFactor);
+    void setBandwidth        (signalBandwidth_t bandwidth); 
+
+    uint8_t buildStringFromSerial (Serial_* dataPort);
+    uint8_t buildStringFromSerial (Uart* dataPort);
+
+    void serviceTx (uint8_t destAddress);
+    void serviceRx ();
+    
+    //sendLinkChangeRequest (uint8_t destAddr;
+    //                       spreadingFactor_t spreadingFactor,
+    //                       signalBandwidth_t signalBandwidth,
+    //                       int8_t txPower);
+    //checkForMessages ();
+    //sendSleepRequest (uint8_t destAddr);
+    //sendWakeRequest (uint8_t destAddr);
+    //goToSleep ();
+  private:
+    //uint8_t (*serviceMsgData)()
+    //uint8_t (*serviceMsgLinkAdapt)()
+    uint8_t thisAddress;
+    uint8_t buildStringFromSerialInner (char inputChar);
+    
+    const uint8_t spreadingFactorTable [NUM_spreadingFactors] = {7, 8, 9, 10, 11, 12};
+    const uint32_t signalBandwidthTable [NUM_signalBandwidths] = {125000, 250000, 500000, 625000};
+    const uint16_t frequencyChannelTable [NUM_frequencyChannels] = {9030, 9046, 9062, 9078, 9094, 9110, 9126, 9142, 9233, 9239, 9245, 9251, 9257, 9263, 9269, 9275};
+    
     RH_RF95 rf95;
+    RHReliableDatagram rhReliableDatagram;
+    
 };
 
-uint8_t serviceMsg (uint8_t* buf,
-                    uint8_t* len,
-                    uint8_t srcAddr,
-                    uint8_t destAddr,
-                    uint8_t id,
-                    uint8_t flags);
+
+//-----------------------
+// Function Declarations
+//-----------------------
 
 #endif // LORA_POINT_2_POINT_PROTOCOL_H
